d9ede62e53d51895920c833c9d9e5f2a3583e05e
Thomashub26 <thomasmondragon44@gmail.com>
2025-11-05 20:56:08 -0500

    v3

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -5,8 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 const HOST = "0.0.0.0";
 
+// Servir archivos estáticos (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname)));
 
+// Ruta principal
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
